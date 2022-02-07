2ebad764e3e679aea4e13eeace527e5f4292b501
Sabrina Couto <93749159+sabrinacouto@users.noreply.github.com>
2022-02-07 15:52:42 -0300

    Update P_3.sql

--- queries/P_3.sql
+++ queries/P_3.sql
@@ -1,1 +1,3 @@
+-- Comparação de quantidade de aparição por episódio de alguns personagens principais:
+
 select nome, aparicao as aparicao_total from personagens where nome in('arya stark', 'Tommen Baratheon', 'Daenerys Targaryen', 'Jon Snow', 'Khal Drogo', 'Margaery Tyrell'); 
