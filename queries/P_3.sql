fb8aca162ba48a224727165e6ca5cb07d60c9392
Sabrina <sabrinacoutolima@gmail.com>
2022-02-07 15:39:00 -0300

    Adicionando comentário no arquivo

--- queries/P_3.sql
+++ queries/P_3.sql
@@ -1,1 +1,3 @@
-select nome, aparicao as aparicao_total from personagens where nome in('arya stark', 'Tommen Baratheon', 'Daenerys Targaryen', 'Jon Snow', 'Khal Drogo', 'Margaery Tyrell'); 
+-- Comparação de quantidade de aparição por episódio de alguns personagens principais:
+
+select nome, aparicao as aparicao_total from personagens where nome in ('Arya Stark', 'Tommen Baratheon', 'Daenerys Targaryen', 'Jon Snow', 'Khal Drogo', 'Margaery Tyrell'); 
